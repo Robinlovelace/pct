cc04f00ca2a9c26e8048997e7b45d13a96f45360
usr110 <aa797@cdpc42.medschl.cam.ac.uk>
2015-02-18 16:28:45 +0000

    -- AddPolygones (which adds boundaries to MSOAs) is only called when the checkbox is CHECKED

--- shiny-test/fixMyPath_basic/server.R
+++ shiny-test/fixMyPath_basic/server.R
@@ -11,46 +11,47 @@ lquiet <- l[ l$color == "red", ]
 flows <- read.csv("al-flow.csv")
 leeds <- readRDS("leeds-msoas-simple.Rds")
 
-shinyServer(function(input, output){
+shinyServer(function(input, output, session){
 
   cents <- coordinates(leeds)
   cents <- SpatialPointsDataFrame(cents, data = leeds@data, match.ID = F)
-
-  map <- leaflet() %>%
-    addTiles(urlTemplate = "http://{s}.tile.thunderforest.com/cycle/{z}/{x}/{y}.png")
 
   zoom <- reactive({
     ifelse(is.null(input$map_zoom),11,input$map_zoom)
   })
 
-  center <- reactive({
-    if(is.null(input$map_bounds)) {
-      c(-1.549167, 53.799722)
-    } else {
-      map_bounds <- input$map_bounds
-      c((map_bounds$north + map_bounds$south)/2.0,(map_bounds$east + map_bounds$west)/2.0)
+  observe({
+    map <- leaflet() %>%
+      addTiles(urlTemplate = "http://{s}.tile.thunderforest.com/cycle/{z}/{x}/{y}.png") %>%
+      addPolylines(data = lfast, color = "red"
+                   , opacity = input$transp_fast
+                   , popup = sprintf("<dl><dt>Distance </dt><dd>%s km</dd><dt>Journeys by bike</dt><dd>%s%%</dd>", round(flows$fastest_distance_in_m / 1000, 1), round(flows$p_cycle * 100, 2))
+      ) %>%
+      addPolylines(data = lquiet, color = "green",
+                   , opacity = input$transp_fast
+                   , popup = sprintf("<dl><dt>Distance </dt><dd>%s km</dd><dt>Journeys by bike</dt><dd>%s%%</dd>", round(flows$quietest_distance_in_m / 1000, 1), round(flows$p_cycle * 100, 2))
+      ) %>%
+      addCircleMarkers(data = cents
+                       , radius = 2
+                       , color = "black"
+                       , popup = sprintf("<b>Journeys by bike: </b>%s%%", round(leeds$pCycle * 100, 2))) %>%
+      addGeoJSON(RJSONIO::fromJSON(sprintf("%s.geojson", input$feature))) %>%
+      mapOptions(zoomToLimits = "first")
+
+    if (input$transp_zones == FALSE){
+      updateSliderInput(session, "transp_fast", value = 0, min = 0, max = 1)
+    }else {
+      updateSliderInput(session, "transp_fast", value = input$transp_fast, min = 0, max = 1)
+      map <- map %>%
+        addPolygons(data = leeds
+                    , fillOpacity = 0.4
+                    , opacity = (input$transp_zones)*.4
+                    , fillColor = leeds$color_pcycle
+        )
     }
+    output$map = renderLeaflet(map)
   })
 
-  output$map = renderLeaflet(map%>%
-                                 addPolygons(data = leeds
-                                             , fillOpacity = 0.4
-                                             , opacity = (input$transp_zones)*.4
-                                             , fillColor = leeds$color_pcycle
-                                 ) %>%
-                                 addPolylines(data = lfast, color = "red"
-                                              , opacity = input$transp_fast
-                                              , popup = sprintf("<dl><dt>Distance </dt><dd>%s km</dd><dt>Journeys by bike</dt><dd>%s%%</dd>", round(flows$fastest_distance_in_m / 1000, 1), round(flows$p_cycle * 100, 2))
-                                 ) %>%
-                                 addPolylines(data = lquiet, color = "green",
-                                              , opacity = input$transp_fast
-                                              , popup = sprintf("<dl><dt>Distance </dt><dd>%s km</dd><dt>Journeys by bike</dt><dd>%s%%</dd>", round(flows$quietest_distance_in_m / 1000, 1), round(flows$p_cycle * 100, 2))
-                                 ) %>%
-                                 addCircleMarkers(data = cents
-                                                  , radius = 2
-                                                  , color = "black"
-                                                  , popup = sprintf("<b>Journeys by bike: </b>%s%%", round(leeds$pCycle * 100, 2))) %>%
-                                 addGeoJSON(RJSONIO::fromJSON(sprintf("%s.geojson", input$feature))) %>%
-                                 mapOptions(zoomToLimits = "first")
-  )
+
+
 })
