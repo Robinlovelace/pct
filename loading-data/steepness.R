c572a801305f470b6f29be884e90e763d8e7b71f
Robinlovelace <rob00x@gmail.com>
2015-04-07 14:52:59 +0100

    Various updates to documents

--- loading-data/steepness.R
+++ loading-data/steepness.R
@@ -28,12 +28,28 @@ lapply(pkgs, library, character.only = TRUE)
 # # # # # # # # # # # # # # # # #
 # Download latest 90m res data  #
 # See http://srtm.csi.cgiar.org/#
+# Select tiles manually         #
 # # # # # # # # # # # # # # # # #
 
+# For Netherlands
+nl <- getData(name = 'GADM', country = "NLD", level = 0)
+library(downloader)
+download("https://github.com/johan/world.geo.json/raw/master/countries/NLD.geo.json", destfile = "/tmp/NLD.geojson")
+library(geojsonio)
+nl <- geojson_read("/tmp/NLD.geojson")
+bbox(nl)
 url <- "ftp://srtm.csi.cgiar.org/SRTM_V41/SRTM_Data_GeoTiff/" # base url
+
+tiles <- c("srtm_37_02.zip", "srtm_38_02.zip")
+for(i in tiles){
+  # download.file(paste0(url, i), destfile = paste0("bigdata/", i), method = "curl")
+  unzip(paste0("bigdata/", i), exdir = "bigdata/")
+}
+
+# For UK
 # which tiles are you interested in?
-tiles <- c("srtm_35_02.zip", "srtm_36_02.zip", "srtm_37_02.zip", "srtm_35_01.zip", "srtm_36_01.zip" )
-paste0(url, tiles)
+# tiles <- c("srtm_35_02.zip", "srtm_36_02.zip", "srtm_37_02.zip", "srtm_35_01.zip", "srtm_36_01.zip" )
+# paste0(url, tiles)
 
 # # Download files in a loop - warning: takes some time!
 # for(i in tiles){
@@ -45,14 +61,16 @@ paste0(url, tiles)
 # Load srtm data #
 # # # # # # # #  #
 
-# f <- list.files(path = "bigdata/", pattern = ".tif", full.names = T)
-# uksrtm <- raster(f[1])
-# for(i in f[-1]){
-#   uksrtm <- raster::merge(uksrtm, raster(i))
-# }
-# proj4string(uksrtm) <- CRS("+init=epsg:4326")
-# uksrtm2 <- projectRaster(uksrtm, crs = CRS("+init=epsg:27700")) # warning: not necessary, computationally intensive
-# uksrtm2 # to find resolution
+f <- list.files(path = "bigdata/", pattern = paste(gsub(".zip", ".tif", tiles), sep = "|", collapse = "|"), full.names = T)
+uksrtm <- raster(f[1])
+for(i in f[-1]){
+  uksrtm <- raster::merge(uksrtm, raster(i))
+}
+
+proj4string(uksrtm) <- CRS("+init=epsg:4326")
+writeRaster(uksrtm, filename = "/tmp/nl-alts.geotiff")
+uksrtm2 <- projectRaster(uksrtm, crs = CRS("+init=epsg:27700")) # warning: not necessary, computationally intensive
+uksrtm2 # to find resolution
 
 # # # # # # # # # #
 # Read local data #
@@ -71,7 +89,8 @@ eng_lsoa_cents <- gCentroid(eng_lsoa, byid = T)
 # # # # # # # # # # # # # #
 
 # ukslope <- terrain(uksrtm, opt = "slope", unit = "degrees")
-# writeRaster(ukslope, filename = "bigdata/ukslope.grd")
+# writeRaster(ukslope, filename = "bigdata/dutchslope.asc")
+# writeRaster(ukslope, filename = "bigdata/dutchslope.tif")
 # ukslope <- raster("bigdata/ukslope.grd")
 # system.time(
 # avslope <- raster::extract(ukslope, eng_lsoa, method = 'bilinear', fun = mean, na.rm = T)
