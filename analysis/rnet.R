5ca582af5b92a014bb5777ed2652ed0e58ec5400
Robinlovelace <rob00x@gmail.com>
2015-09-15 00:44:34 +0100

    Update R network analysis test script

--- analysis/rnet.R
+++ analysis/rnet.R
@@ -1,15 +1,8 @@
+# Aim: explore generation and visualisation of the network
+# Next step: develop a proper script, to be called from load.Rmd that does this
+# for each region
+
 library(stplanr)
-
-rf <- route_cyclestreet(from = "Dublin", to = "Maynooth", plan = "fastest")
-plot(rf)
-
-source("~/repos/stplanr/R/qleafmap.R")
-
-qleafmap(rf)
-
-rf2 <- route_graphhopper(from = "Dublin", to = "Maynooth", vehicle = "car")
-
-rf2@data
 
 l <- readRDS("~/repos/pct/pct-data/leeds/l.Rds")
 rfall <- readRDS("~/repos/pct/pct-data/leeds/rf.Rds")
@@ -26,22 +19,25 @@ plot(rfall)
 
 plot(rfall)
 
-t10 <- rfall[1:20,]
+t10 <- rfall
 plot(t10)
 head(t10@data)
-rg10 <- gOverline(t10, attrib = "gov_target")
+rg <- gOverline(t10, attrib = "gov_target")
+
+# plot with width proportional to olc
+leaflet()  %>% addPolylines(data = t10, weight = t10$current/1.5, opacity = 0.2)
 
 # rg <- gOverline(rfall, attrib = "gov_target")
-rg <- readRDS("~/repos/pct/pct-data/leeds/rnet.RData")
+# rg <- readRDS("~/repos/pct/pct-data/leeds/rnet.RData")
 
-line_widths <- rg@data$gov_target / 100
+summary(rg)
+line_widths <- rg$gov_target / mean(rg$gov_target) * 3
+summary(line_widths)
 line_widths <- line_widths + 0.3
 summary(line_widths)
 line_widths[line_widths > 8] <- 8
 
 leaflet() %>% addTiles() %>% addPolylines(data = rg, weight = line_widths, popup = rg@data$gov_target)
-
-rg <-
 
 plot(rg, lwd = rg@data$gov_target / 500)
 # saveRDS(rg, "~/repos/pct/pct-data/leeds/rnet.RData")
