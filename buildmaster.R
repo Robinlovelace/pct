7423a84bc6d92783d4e8d8af5cd799485d8d312f
Robinlovelace <rob00x@gmail.com>
2015-09-26 17:21:12 +0100

    Fix bug with some CycleStreets lines and loop over CUAs

--- buildmaster.R
+++ buildmaster.R
@@ -1,8 +1,13 @@
-la <- "coventry"
-
 library(knitr)
-knitr::knit2html(
-  input = "load.Rmd",
-  output = file.path("pct-data", la, "model-output.html"),
-  envir = globalenv()
+
+la_all <- c("devon", "norfolk")
+
+for(la in la_all){
+  knitr::knit2html(
+    input = "load.Rmd",
+    output = file.path("pct-data", la, "model-output.html"),
+    envir = globalenv()
   )
+}
+
+
