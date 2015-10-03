ee1e9e215284dbbf30daf94b9771467f3a481a15
Robinlovelace <rob00x@gmail.com>
2015-10-03 09:20:12 +0100

    Add additional comments in buildmaster.R

--- buildmaster.R
+++ buildmaster.R
@@ -1,8 +1,6 @@
 library(knitr)
 
-la_all <-
-   c("Bolton|Bury|Manchester|Oldham|Rochdale|Salford|Stockport|Tameside|Trafford|Wigan",
-     "Blackpool")
+la_all <- c("Barnet", "Barnsley", "Bath and North East Somerset", "Bedford")
 
 for(i in la_all){
   la <- i
@@ -11,6 +9,17 @@ for(i in la_all){
     output = file.path("pct-data/", la, "/model-output.html"),
     envir = globalenv()
   )
+  # Note: copy directories into pct-shiny (commented out as in load.Rmd)
+  # mess <- paste0("cp -r ../pct-shiny/coventry ../pct-shiny/", la)
+  # server_text <- readLines("../pct-shiny/coventry/server.R")
+  # server_text <- gsub(pattern = "coventry", replacement = la, x = server_text)
+  # writeLines(server_text, con = paste0("../pct-shiny/", la, "/server.R"))
 }
 
 
+# las <- readOGR(dsn = "pct-bigdata/national/cuas-mf.geojson", layer = "OGRGeoJSON")
+# las_names <- las$CTYUA12NM
+# las_names <- las_names[order(las_names)]
+# las_names <- as.character(las_names)
+# head(las_names)
+# dput(las_names[2:5])
