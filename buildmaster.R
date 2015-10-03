a687791f21f2dd2b74cf8cee661397f3ace4d03b
Robinlovelace <rob00x@gmail.com>
2015-10-03 02:53:39 +0100

    Update load.Rmd for Greater Manchester

--- buildmaster.R
+++ buildmaster.R
@@ -1,11 +1,14 @@
 library(knitr)
 
-la_all <- c("devon", "norfolk")
+la_all <-
+   c("Bolton|Bury|Manchester|Oldham|Rochdale|Salford|Stockport|Tameside|Trafford|Wigan",
+     "Blackpool")
 
-for(la in la_all){
+for(i in la_all){
+  la <- i
   knitr::knit2html(
     input = "load.Rmd",
-    output = file.path("pct-data", la, "model-output.html"),
+    output = file.path("pct-data/", la, "/model-output.html"),
     envir = globalenv()
   )
 }
